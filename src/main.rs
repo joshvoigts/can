23c02add391ecb98aaf9741e482ce6234c164e83
Josh Voigts <joshvoigts@gmail.com>
2025-12-22 09:36:31 -0600

    Add rustfml.toml

--- src/main.rs
+++ src/main.rs
@@ -12,224 +12,221 @@ mod opt;
 mod p;
 
 fn main() {
-   let mut opts = Opts::new();
-   opts.insert(
-      "help",
-      Opt {
-         description: "Show help",
-         enabled: false,
-         handler: Some(help),
-         long: "--help",
-         short: "-h",
-      },
-   );
-   opts.insert(
-      "empty",
-      Opt {
-         description: "Empty trash",
-         enabled: false,
-         handler: Some(empty),
-         long: "--empty",
-         short: "-e",
-      },
-   );
-   opts.insert(
-      "list",
-      Opt {
-         description: "List files in trash",
-         enabled: false,
-         handler: Some(list),
-         long: "--list",
-         short: "-l",
-      },
-   );
-   opts.insert(
-      "verbose",
-      Opt {
-         description: "Run verbosely",
-         enabled: false,
-         handler: None,
-         long: "--verbose",
-         short: "-v",
-      },
-   );
+  let mut opts = Opts::new();
+  opts.insert(
+    "help",
+    Opt {
+      description: "Show help",
+      enabled: false,
+      handler: Some(help),
+      long: "--help",
+      short: "-h",
+    },
+  );
+  opts.insert(
+    "empty",
+    Opt {
+      description: "Empty trash",
+      enabled: false,
+      handler: Some(empty),
+      long: "--empty",
+      short: "-e",
+    },
+  );
+  opts.insert(
+    "list",
+    Opt {
+      description: "List files in trash",
+      enabled: false,
+      handler: Some(list),
+      long: "--list",
+      short: "-l",
+    },
+  );
+  opts.insert(
+    "verbose",
+    Opt {
+      description: "Run verbosely",
+      enabled: false,
+      handler: None,
+      long: "--verbose",
+      short: "-v",
+    },
+  );
 
-   let mut args: BTreeSet<String> = env::args().skip(1).collect();
+  let mut args: BTreeSet<String> = env::args().skip(1).collect();
 
-   for mut opt in opts.values_mut() {
-      if args.remove(opt.short) {
-         opt.enabled = true;
-      }
-      if args.remove(opt.long) {
-         opt.enabled = true;
-      }
-   }
+  for mut opt in opts.values_mut() {
+    if args.remove(opt.short) {
+      opt.enabled = true;
+    }
+    if args.remove(opt.long) {
+      opt.enabled = true;
+    }
+  }
 
-   for opt in opts.values() {
-      if !opt.enabled {
-         continue;
-      }
-      if let Some(handler) = opt.handler {
-         handler(&opts);
-         process::exit(0);
-      }
-   }
+  for opt in opts.values() {
+    if !opt.enabled {
+      continue;
+    }
+    if let Some(handler) = opt.handler {
+      handler(&opts);
+      process::exit(0);
+    }
+  }
 
-   if args.len() > 0 {
-      move_files_to_trash(&opts, &args);
-   }
+  if args.len() > 0 {
+    move_files_to_trash(&opts, &args);
+  }
 
-   help(&opts);
+  help(&opts);
 }
 
 fn help(opts: &Opts) {
-   println!("Usage: can [options] file ...");
-   for opt in opts.values() {
-      println!(
-         "  {}, {:<10} {}",
-         opt.short, opt.long, opt.description
-      )
-   }
+  println!("Usage: can [options] file ...");
+  for opt in opts.values() {
+    println!("  {}, {:<10} {}", opt.short, opt.long, opt.description)
+  }
 }
 
 fn empty(opts: &Opts) {
-   match env::consts::OS {
-      "macos" => {
-         let as_cmd = "tell application \"Finder\" to empty trash";
-         let res = run_applescript(as_cmd.to_string());
-         match res {
-            Ok(_) => {
-               if let Some(opt) = opts.get("verbose") {
-                  if opt.enabled {
-                     println!("Trash emptied");
-                  }
-               }
+  match env::consts::OS {
+    "macos" => {
+      let as_cmd = "tell application \"Finder\" to empty trash";
+      let res = run_applescript(as_cmd.to_string());
+      match res {
+        Ok(_) => {
+          if let Some(opt) = opts.get("verbose") {
+            if opt.enabled {
+              println!("Trash emptied");
             }
-            Err(_) => (), // Ignore, trash probably already empty
-         }
+          }
+        }
+        Err(_) => (), // Ignore, trash probably already empty
       }
-      _ => fail!("can: OS not supported"),
-   }
+    }
+    _ => fail!("can: OS not supported"),
+  }
 }
 
 fn list(_opts: &Opts) {
-   let files = get_files(&get_trash_path());
-   for file in files {
-      match file.file_name().into_string() {
-         Ok(name) => println!("{}", name),
-         Err(_) => fail!("can: Could not list non-unicode filename"),
-      }
-   }
+  let files = get_files(&get_trash_path());
+  for file in files {
+    match file.file_name().into_string() {
+      Ok(name) => println!("{}", name),
+      Err(_) => fail!("can: Could not list non-unicode filename"),
+    }
+  }
 }
 
 fn get_files(dir_path: &Path) -> Vec<DirEntry> {
-   if !dir_path.exists() || !dir_path.is_dir() {
-      fail!("can: Could not find trash folder");
-   }
-   let mut files: Vec<DirEntry> = Vec::new();
-   match fs::read_dir(dir_path) {
-      Ok(entries) => {
-         for maybe_entry in entries {
-            match maybe_entry {
-               Ok(entry) => {
-                  let maybe_name = entry.file_name().into_string();
-                  if let Ok(file_name) = maybe_name {
-                     if !file_name.starts_with(".") {
-                        files.push(entry);
-                     }
-                  }
-               }
-               Err(err) => {
-                  fail!("can: Could not access trash file: {}", err)
-               }
+  if !dir_path.exists() || !dir_path.is_dir() {
+    fail!("can: Could not find trash folder");
+  }
+  let mut files: Vec<DirEntry> = Vec::new();
+  match fs::read_dir(dir_path) {
+    Ok(entries) => {
+      for maybe_entry in entries {
+        match maybe_entry {
+          Ok(entry) => {
+            let maybe_name = entry.file_name().into_string();
+            if let Ok(file_name) = maybe_name {
+              if !file_name.starts_with(".") {
+                files.push(entry);
+              }
             }
-         }
+          }
+          Err(err) => {
+            fail!("can: Could not access trash file: {}", err)
+          }
+        }
       }
-      Err(err) => {
-         fail!("can: Could not access trash directory: {}", err)
-      }
-   }
-   files.sort_by_key(|entry| {
-      entry.metadata().unwrap().modified().unwrap()
-   });
-   return files;
+    }
+    Err(err) => {
+      fail!("can: Could not access trash directory: {}", err)
+    }
+  }
+  files.sort_by_key(|entry| {
+    entry.metadata().unwrap().modified().unwrap()
+  });
+  return files;
 }
 
 fn get_trash_path() -> PathBuf {
-   match env::var("HOME") {
-      Ok(home) => match env::consts::OS {
-         "macos" => {
-            let mut path_buf = PathBuf::new();
-            path_buf.push(&home);
-            path_buf.push(".Trash");
-            path_buf
-         }
-         _ => {
-            let mut path_buf = PathBuf::new();
-            path_buf.push(&home);
-            path_buf.push(".local");
-            path_buf.push("share");
-            path_buf.push("Trash");
-            path_buf
-         }
-      },
-      Err(_) => panic!("can: Could not find trash folder"),
-   }
+  match env::var("HOME") {
+    Ok(home) => match env::consts::OS {
+      "macos" => {
+        let mut path_buf = PathBuf::new();
+        path_buf.push(&home);
+        path_buf.push(".Trash");
+        path_buf
+      }
+      _ => {
+        let mut path_buf = PathBuf::new();
+        path_buf.push(&home);
+        path_buf.push(".local");
+        path_buf.push("share");
+        path_buf.push("Trash");
+        path_buf
+      }
+    },
+    Err(_) => panic!("can: Could not find trash folder"),
+  }
 }
 
 fn move_files_to_trash(opts: &Opts, args: &BTreeSet<String>) {
-   let mut to_delete: Vec<String> = Vec::new();
-   for arg in args {
-      let path = Path::new(&arg);
-      if !path.exists() {
-         fail!("can: {}: No such file or directory", arg);
+  let mut to_delete: Vec<String> = Vec::new();
+  for arg in args {
+    let path = Path::new(&arg);
+    if !path.exists() {
+      fail!("can: {}: No such file or directory", arg);
+    }
+    match fs::canonicalize(path) {
+      Ok(abs_path) => {
+        let abs_str = abs_path.display().to_string();
+        if abs_str.contains("\"") {
+          fail!("can: {}: Could not escape path", arg);
+        }
+        to_delete.push(abs_str);
       }
-      match fs::canonicalize(path) {
-         Ok(abs_path) => {
-            let abs_str = abs_path.display().to_string();
-            if abs_str.contains("\"") {
-               fail!("can: {}: Could not escape path", arg);
-            }
-            to_delete.push(abs_str);
-         }
-         Err(_) => fail!("can: {}: Could not canonicalize", arg),
+      Err(_) => fail!("can: {}: Could not canonicalize", arg),
+    }
+  }
+  match env::consts::OS {
+    "macos" => {
+      let mut as_list = "{ POSIX file \"".to_owned();
+      as_list.push_str(&to_delete.join("\", POSIX file \""));
+      as_list.push_str("\"}");
+      let mut as_cmd =
+        "tell application \"Finder\" to delete ".to_owned();
+      as_cmd.push_str(&as_list);
+      let res = run_applescript(as_cmd);
+      if let Err(err) = res {
+        fail!("can: Applescript error: {}", err);
       }
-   }
-   match env::consts::OS {
-      "macos" => {
-         let mut as_list = "{ POSIX file \"".to_owned();
-         as_list.push_str(&to_delete.join("\", POSIX file \""));
-         as_list.push_str("\"}");
-         let mut as_cmd =
-            "tell application \"Finder\" to delete ".to_owned();
-         as_cmd.push_str(&as_list);
-         let res = run_applescript(as_cmd);
-         if let Err(err) = res {
-            fail!("can: Applescript error: {}", err);
-         }
+    }
+    _ => fail!("can: OS not supported"),
+  }
+  if let Some(opt) = opts.get("verbose") {
+    if opt.enabled {
+      for arg in args {
+        println!("{}", arg);
       }
-      _ => fail!("can: OS not supported"),
-   }
-   if let Some(opt) = opts.get("verbose") {
-      if opt.enabled {
-         for arg in args {
-            println!("{}", arg);
-         }
-      }
-   }
-   process::exit(0);
+    }
+  }
+  process::exit(0);
 }
 
 fn run_applescript(as_cmd: String) -> Result<String, String> {
-   let res = Command::new("osascript").args(["-e", &as_cmd]).output();
-   match res {
-      Ok(output) => {
-         if output.stderr.len() > 0 {
-            let err = from_utf8(&output.stderr).unwrap().to_owned();
-            return Err(err);
-         }
-         return Ok(from_utf8(&output.stdout).unwrap().to_owned());
+  let res = Command::new("osascript").args(["-e", &as_cmd]).output();
+  match res {
+    Ok(output) => {
+      if output.stderr.len() > 0 {
+        let err = from_utf8(&output.stderr).unwrap().to_owned();
+        return Err(err);
       }
-      Err(err) => return Err(err.to_string()),
-   }
+      return Ok(from_utf8(&output.stdout).unwrap().to_owned());
+    }
+    Err(err) => return Err(err.to_string()),
+  }
 }
--- src/opt.rs
+++ src/opt.rs
@@ -5,20 +5,20 @@ pub type Opts = HashMap<&'static str, Opt>;
 
 #[derive(Clone)]
 pub struct Opt {
-   pub description: &'static str,
-   pub enabled: bool,
-   pub handler: Option<fn(&Opts)>,
-   pub long: &'static str,
-   pub short: &'static str,
+  pub description: &'static str,
+  pub enabled: bool,
+  pub handler: Option<fn(&Opts)>,
+  pub long: &'static str,
+  pub short: &'static str,
 }
 
 impl fmt::Debug for Opt {
-   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-      f.debug_struct("Opt")
-         .field("description", &self.description)
-         .field("handler", &"handler")
-         .field("long", &self.long)
-         .field("short", &self.short)
-         .finish()
-   }
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    f.debug_struct("Opt")
+      .field("description", &self.description)
+      .field("handler", &"handler")
+      .field("long", &self.long)
+      .field("short", &self.short)
+      .finish()
+  }
 }
